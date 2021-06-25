e325a416e2973725bc652fa9d6af35e3bdc3e1cb
Lipa Long <lipalong@Lipas-MacBook-Pro.local>
2021-06-25 13:16:33 -0700

    moved call to AppPassLogin

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -15,7 +15,6 @@ import { exit_and_close } from '../../modules/message';
 import useLocale from '../../hooks/useLocale';
 import AppModalDialogs from './AppModalDialogs';
 import AppLoading from './AppLoading';
-import AppPassLogin from './AppPassLogin';
 import { i18n, activateLocale, defaultLocale, getMaterialLocale } from '../../config/locales';
 import Fonts from './fonts/Fonts';
 
@@ -79,7 +78,6 @@ export default function App() {
               <AppRouter />
               <AppModalDialogs />
               <AppLoading />
-              <AppPassLogin />
             </ThemeProvider>
           </WebSocketConnection>
         </I18nProvider>
