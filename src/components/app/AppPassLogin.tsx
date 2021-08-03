3164f385bbf9aafa42d7d85a6e198181a3ac9dd0
Jeff Cruikshank <jeff@chia.net>
2021-08-03 14:41:21 -0700

    Renamed state props relating to keyring passphrase state. Fixed some compiler warnings.

--- src/components/app/AppPassLogin.tsx
+++ src/components/app/AppPassLogin.tsx
@@ -15,50 +15,50 @@ import { Trans } from '@lingui/macro';
 import { AlertDialog } from '@chia/core';
 import { openDialog } from '../../modules/dialog';
 import { unlockKeyring } from '../../modules/daemon_messages';
+import { RootState } from 'modules/rootReducer';
 
 export default function AppPassLogin() {
   const dispatch = useDispatch();
-  let passphrase_status = useSelector((state) => state.daemon_state.passphrase_status);
-  let passphrase_lock_status = useSelector((state) => state.daemon_state.passphrase_lock_status);
-  let unlock_in_progress = useSelector((state) => state.daemon_state.keyring_unlock_in_progress);
+  let user_passphrase_is_set = useSelector((state: RootState) => state.daemon_state.keyring_user_passphrase_set);
+  let keyring_locked = useSelector((state: RootState) => state.daemon_state.keyring_locked);
+  let unlock_in_progress = useSelector((state: RootState) => state.daemon_state.keyring_unlock_in_progress);
+  let passphrase_input: any = null;
+
 
   console.log("APP PASS LOGIN");
-  console.log(passphrase_status);
-  console.log(passphrase_lock_status);
+  console.log("user_passphrase_is_set: " + user_passphrase_is_set);
+  console.log("keyring_locked: " + keyring_locked);
   console.log("unlock_in_progress: " + unlock_in_progress);
-  if (passphrase_status) {
-    const open = true
 
-    let passphrase_input = null;
-
-    function handleSubmit() {
-      if (
-        passphrase_input.value === ''
-      ) {
-        dispatch(
-          openDialog(
-            <AlertDialog>
-              <Trans>
-                Please enter a passphrase
-              </Trans>
-            </AlertDialog>
-          ),
-        );
-        return;
-      }
-      dispatch(unlockKeyring(passphrase_input.value));
+  function handleSubmit() {
+    if (
+      passphrase_input.value === ''
+    ) {
+      dispatch(
+        openDialog(
+          <AlertDialog>
+            <Trans>
+              Please enter a passphrase
+            </Trans>
+          </AlertDialog>
+        ),
+      );
+      return;
     }
+    dispatch(unlockKeyring(passphrase_input.value));
+  }
 
-    function handleKeyDown(e) {
-      if (e.key === 'Enter') {
-        handleSubmit();
-      }
+  function handleKeyDown(e) {
+    if (e.key === 'Enter') {
+      handleSubmit();
     }
+  }
 
+  if (user_passphrase_is_set) {
     return (
       <div>
         <Dialog
-          open={open}
+          open={true}
           aria-labelledby="form-dialog-title"
           fullWidth={true}
           maxWidth = {'xs'}
@@ -93,7 +93,7 @@ export default function AppPassLogin() {
           </DialogActions>
         </Dialog>
       </div>
-    )
+    );
   }
 
   return null;
