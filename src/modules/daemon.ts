f09f02f15e69482798c86d8789b2cc6cad7a62ec
Jeff Cruikshank <jeff@chia.net>
2021-08-10 22:44:07 +0000

    Moved remaining keyring state out of DaemonState and into KeyringState. Handle bad passphrase when unlocking the keyring. Minor fix to the change passphrase logic (new passphrase and confirmation should match).

--- src/modules/daemon.ts
+++ src/modules/daemon.ts
@@ -21,12 +21,6 @@ type DeamonState = {
   harvester_connected: boolean;
   plotter_running: boolean;
   exiting: boolean;
-  keyring_locked: boolean;
-  keyring_unlock_bad_passphrase: boolean;
-  keyring_unlock_in_progress: boolean;
-  keyring_passphrase_support_enabled: boolean;
-  keyring_user_passphrase_set: boolean;
-  keyring_needs_migration: boolean;
 };
 
 const initialState: DeamonState = {
@@ -42,12 +36,6 @@ const initialState: DeamonState = {
   harvester_connected: false,
   plotter_running: false,
   exiting: false,
-  keyring_locked: false,
-  keyring_unlock_bad_passphrase: false,
-  keyring_unlock_in_progress: false,
-  keyring_passphrase_support_enabled: false,
-  keyring_user_passphrase_set: false,
-  keyring_needs_migration: false,
 };
 
 export default function daemonReducer(
@@ -127,57 +115,6 @@ export default function daemonReducer(
             return { ...state, plotter_running: false };
           }
         }
-      } else if (command === 'is_keyring_locked') {
-        let success = data.success;
-        if (success) {
-          const { is_keyring_locked } = data;
-          return {
-            ...state,
-            keyring_locked: is_keyring_locked,
-          };
-        }
-      } else if (command === 'keyring_status') {
-        let success = data.success;
-        console.log("INCOMING SUCCESS")
-        console.log(success)
-        if (success) {
-          console.log("INCOMING DATA")
-          console.log(data)
-          const { is_keyring_locked } = data;
-          const { passphrase_support_enabled } = data;
-          const { user_passphrase_is_set } = data;
-          const { needs_migration } = data;
-          return {
-            ...state,
-            keyring_locked: is_keyring_locked,
-            keyring_passphrase_support_enabled: passphrase_support_enabled,
-            keyring_user_passphrase_set: user_passphrase_is_set,
-            keyring_needs_migration: needs_migration,
-          };
-        }
-      } else if (command === 'unlock_keyring') {
-        // Clear the keyring_unlock_in_progress flag
-        state = { ...state, keyring_unlock_in_progress: false };
-        if (data.success) {
-          console.log("Keyring was successfully unlocked");
-          return {
-            ...state,
-            keyring_locked: false,
-            keyring_unlock_bad_passphrase: false,
-          };
-        }
-        else {
-          if (data.error === 'bad passphrase') {
-            console.log("Keyring passphrase is invalid");
-            return {
-              ...state,
-              keyring_unlock_bad_passphrase: true,
-            };
-          }
-          else {
-            console.log("Failed to unlock keyring: " + data.error);
-          }
-        }
       }
       return state;
     case 'OUTGOING_MESSAGE':
@@ -186,13 +123,6 @@ export default function daemonReducer(
         action.message.destination === 'daemon'
       ) {
         return { ...state, exiting: true };
-      }
-      else if (
-        action.message.command === 'unlock_keyring' &&
-        action.message.destination === 'daemon'
-      ) {
-        // Set a flag indicating that we're attempting to unlock the keyring
-        return { ...state, keyring_unlock_in_progress: true };
       }
       return state;
     case 'WS_DISCONNECTED':
