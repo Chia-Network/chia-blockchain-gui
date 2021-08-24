6915f5f06878b4b8effcdd1274b2738eb3ec21ec
Lipa Long <lipalong@Lipas-MacBook-Pro.local>
2021-08-24 11:11:03 -0700

    added Settings icon in gui sidebar

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -5,6 +5,7 @@ export { default as Keys } from './Keys';
 export { default as Plot } from './Plot';
 export { default as Pool } from './Pool';
 export { default as PlotHero } from './PlotHero';
+export { default as Settings } from './Settings';
 export { default as Status } from './Status';
 export { default as Trade } from './Trade';
 export { default as Wallet } from './Wallet';
