76f9e6cea9f58a30984580b594631f3ae2679752
Amine Khaldi <amine.khaldi@reactos.org>
2021-09-03 19:50:43 +0100

    Temporarily hardcode multipleWallets to true.

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,7 @@
 export default {
-  multipleWallets: process.env.MULTIPLE_WALLETS === 'true',
+  // FIXME: Temporarily hardcoded to true
+  // multipleWallets: process.env.MULTIPLE_WALLETS === 'true',
+  multipleWallets: true,
   local_test: process.env.LOCAL_TEST === 'true',
   backup_host: 'https://backup.chia.net',
 };
