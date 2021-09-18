9758b7a1496f9eb00b4de25e4c577bf81a67ca50
Jack Nelson <jack@jacknelson.xyz>
2021-09-18 10:32:15 -0400

    downgrade storage and fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     "airbnb-typescript",
+    "airbnb/hooks",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:eslint-comments/recommended",
