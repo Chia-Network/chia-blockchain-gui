b9fb584d4d05edac578864ad6038220f2eac4bc9
Zlatko <zfedor@gmail.com>
2021-11-26 19:45:48 +0100

    moved wallets components to wallets package fixed chiaformatter and use big numbers everywhere

--- packages/core/src/components/Amount/Amount.tsx
+++ packages/core/src/components/Amount/Amount.tsx
@@ -9,7 +9,7 @@ import {
 } from '@material-ui/core';
 import { useWatch, useFormContext } from 'react-hook-form';
 import TextField, { TextFieldProps } from '../TextField';
-import { chia_to_mojo } from '../../utils/chia';
+import chiaToMojo from '../../utils/chiaToMojo';
 import useCurrencyCode from '../../hooks/useCurrencyCode';
 import FormatLargeNumber from '../FormatLargeNumber';
 import Flex from '../Flex';
@@ -54,7 +54,7 @@ export default function Amount(props: AmountProps) {
     name,
   });
 
-  const mojo = chia_to_mojo(value);
+  const mojo = chiaToMojo(value);
 
   return (
     <FormControl variant={variant} fullWidth={fullWidth}>
@@ -75,7 +75,7 @@ export default function Amount(props: AmountProps) {
         <FormHelperText component='div' >
           <Flex alignItems="center" gap={2}>
             <Flex flexGrow={1} gap={1}>
-              {!!mojo && (
+              {!!value && (
                 <>
                   <FormatLargeNumber value={mojo} />
                   <Box>
--- packages/wallet/src/components/wallet/Wallets.tsx
+++ packages/wallet/src/components/wallet/Wallets.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Trans } from '@lingui/macro';
+import { WalletType } from '@chia/api';
 import { useGetWalletsQuery } from '@chia/api-react';
-import StandardWallet from './standard/WalletStandard';
-import { CreateWalletView } from './create/WalletCreate';
-import WalletCAT from './cat/WalletCAT';
-// import RateLimitedWallet from './rateLimited/WalletRateLimited';
-// import DistributedWallet from './did/WalletDID';
-import WalletType from '../../constants/WalletType';
+import { WalletStandard, WalletCAT, WalletCreate } from '@chia/wallets';
 import LayoutMain from '../layout/LayoutMain';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
-import WalletsList from './WalletsList';
 
 export default function Wallets() {
   const { path } = useRouteMatch();
@@ -22,13 +17,10 @@ export default function Wallets() {
       title={<Trans>Wallets</Trans>}
     >
       <Switch>
-        <Route path="/dashboard/wallets" exact>
-          <WalletsList />
-        </Route>
         {wallets?.map((wallet) => (
           <Route path={`${path}/${wallet.id}`} key={wallet.id}>
             {wallet.type === WalletType.STANDARD_WALLET && (
-              <StandardWallet walletId={wallet.id} />
+              <WalletStandard walletId={wallet.id} />
             )}
 
             {wallet.type === WalletType.CAT && (
@@ -45,7 +37,7 @@ export default function Wallets() {
           </Route>
         ))}
         <Route path={`/dashboard/wallets/create`}>
-          <CreateWalletView />
+          <WalletCreate />
         </Route>
       </Switch>
     </LayoutMain>
