2cf35a7bb8f4f47770e2d63e63eb0dfbc8d63cbb
Zlatko <zfedor@gmail.com>
2021-11-28 15:58:08 +0100

    consolidate locales

--- packages/wallet/src/components/layout/LayoutHero.tsx
+++ packages/wallet/src/components/layout/LayoutHero.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from 'react';
 import { AppBar, Toolbar, Box } from '@material-ui/core';
 import styled from 'styled-components';
 import { Flex, DarkModeToggle, LocaleToggle } from '@chia/core';
-import { defaultLocale, locales } from '../../config/locales';
 
 const StyledWrapper = styled(Box)`
   padding-top: ${({ theme }) => `${theme.spacing(3)}px`};
@@ -37,7 +36,7 @@ export default function LayoutHero(props: Props) {
         <Toolbar>
           {header}
           <Flex flexGrow={1} />
-          <LocaleToggle locales={locales} defaultLocale={defaultLocale} />
+          <LocaleToggle />
           <DarkModeToggle />
         </Toolbar>
       </AppBar>
