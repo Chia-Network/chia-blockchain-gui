97cca220cb7a2800e20efca7a21731d184e27c71
Lipa Long <lipalong@Lipas-MacBook-Pro.local>
2022-02-16 11:57:50 -0800

    preliminary balance display testing

--- packages/wallets/src/components/WalletCards.tsx
+++ packages/wallets/src/components/WalletCards.tsx
@@ -1,66 +1,26 @@
 import React, { ReactElement } from 'react';
 import { Grid } from '@material-ui/core';
-import WalletCardTotalBalance from './card/WalletCardTotalBalance';
-import WalletCardSpendableBalance from './card/WalletCardSpendableBalance';
 import WalletCardPendingTotalBalance from './card/WalletCardPendingTotalBalance';
-import WalletCardPendingBalance from './card/WalletCardPendingBalance';
-import WalletCardPendingChange from './card/WalletCardPendingChange';
 
 type Props = {
   walletId: number;
-  totalBalanceTooltip?: ReactElement<any>;
-  spendableBalanceTooltip?: ReactElement<any>;
   pendingTotalBalanceTooltip?: ReactElement<any>;
-  pendingBalanceTooltip?: ReactElement<any>;
-  pendingChangeTooltip?: ReactElement<any>;
 };
 
 export default function WalletCards(props: Props) {
   const {
     walletId,
-    totalBalanceTooltip,
-    spendableBalanceTooltip,
     pendingTotalBalanceTooltip,
-    pendingBalanceTooltip,
-    pendingChangeTooltip,
   } = props;
 
   return (
     <div>
       <Grid spacing={3} alignItems="stretch" container>
-        <Grid xs={12} md={4} item>
-          <WalletCardTotalBalance 
+        <Grid xs={12} item>
+          <WalletCardPendingTotalBalance
             walletId={walletId}
-            tooltip={totalBalanceTooltip}
+            tooltip={pendingTotalBalanceTooltip}
           />
-        </Grid>
-        <Grid xs={12} md={8} item>
-          <Grid spacing={3} alignItems="stretch" container>
-            <Grid xs={12} sm={6} item>
-              <WalletCardSpendableBalance 
-                walletId={walletId}
-                tooltip={spendableBalanceTooltip}
-              />
-            </Grid>
-            <Grid xs={12} sm={6} item>
-              <WalletCardPendingTotalBalance 
-                walletId={walletId}
-                tooltip={pendingTotalBalanceTooltip}
-              />
-            </Grid>
-            <Grid xs={12} sm={6} item>
-              <WalletCardPendingBalance
-                walletId={walletId}
-                tooltip={pendingBalanceTooltip}
-              />
-            </Grid>
-            <Grid xs={12} sm={6} item>
-              <WalletCardPendingChange 
-                walletId={walletId}
-                tooltip={pendingChangeTooltip}
-              />
-            </Grid>
-          </Grid>
         </Grid>
       </Grid>
     </div>
@@ -68,9 +28,5 @@ export default function WalletCards(props: Props) {
 }
 
 WalletCards.defaultProps = {
-  totalBalanceTooltip: undefined,
-  spendableBalanceTooltip: undefined,
   pendingTotalBalanceTooltip: undefined,
-  pendingBalanceTooltip: undefined,
-  pendingChangeTooltip: undefined,
 };
--- packages/wallets/src/components/card/WalletCardPendingTotalBalance.tsx
+++ packages/wallets/src/components/card/WalletCardPendingTotalBalance.tsx
@@ -1,9 +1,12 @@
 import React, { useMemo, ReactElement } from 'react';
 import { Trans } from '@lingui/macro';
 import { useGetWalletBalanceQuery } from '@chia/api-react';
-import { CardSimple } from '@chia/core';
+import { Card, Grid, Box, Flex, CardSimple, TooltipIcon, Loading } from '@chia/core';
+import { Typography } from '@material-ui/core';
 import useWallet from '../../hooks/useWallet';
 import getWalletHumanValue from '../../utils/getWalletHumanValue';
+
+
 
 type Props = {
   walletId: number;
@@ -13,8 +16,8 @@ type Props = {
 export default function WalletCardPendingTotalBalance(props: Props) {
   const { walletId, tooltip } = props;
 
-  const { 
-    data: walletBalance, 
+  const {
+    data: walletBalance,
     isLoading: isLoadingWalletBalance,
     error,
   } = useGetWalletBalanceQuery({
@@ -27,18 +30,44 @@ export default function WalletCardPendingTotalBalance(props: Props) {
   const value = walletBalance?.pendingTotalBalance;
 
   const humanValue = useMemo(() => wallet && value !== undefined
-      ? `${getWalletHumanValue(wallet, value)} ${unit}`
+      ? `${getWalletHumanValue(wallet, value)}`
       : ''
   ,[value, wallet, unit]);
 
+  console.log("yes")
+
   return (
-    <CardSimple
-      loading={isLoading}
-      valueColor="secondary"
-      title={<Trans>Pending Total Balance</Trans>}
+    <Card
+      title={<Trans>Balance</Trans>}
       tooltip={tooltip}
-      value={humanValue}
-      error={error}
-    />
+    >
+      {isLoading ? (
+        <Loading center />
+      ) : error ? (
+        <Flex alignItems="center">
+          <Trans><Typography variant="subtitle1" color="error">Error</Typography></Trans>
+          &nbsp;
+          <TooltipIcon>{error?.message}</TooltipIcon>
+        </Flex>
+      ) : (
+        <div style={{display:"flex", alignItems:"baseline", gap:"4px"}}>
+          <Trans>
+          <Typography variant="h5" style={{ fontWeight: 500 }} color="textPrimary">{humanValue}</Typography>
+          <Typography variant="h7" color="textSecondary">{unit}</Typography>
+          </Trans>
+        </div>
+      )}
+    </Card>
   );
 }
+
+/*
+<CardSimple
+  loading={isLoading}
+  valueColor="secondary"
+  title={<Trans>Pending Total Balance</Trans>}
+  tooltip={tooltip}
+  value={humanValue}
+  error={error}
+/>
+*/
