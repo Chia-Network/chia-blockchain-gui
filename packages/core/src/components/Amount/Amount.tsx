19b64b07400ce5ae57e8e48ebe00a8a1aef25ae1
ChiaMineJP <admin@chiamine.jp>
2022-02-27 00:03:50 +0900

    Fixed a crash after pasting malicious string to amount/fee input on wallets page

--- packages/core/src/components/Amount/Amount.tsx
+++ packages/core/src/components/Amount/Amount.tsx
@@ -16,16 +16,75 @@ import FormatLargeNumber from '../FormatLargeNumber';
 import Flex from '../Flex';
 
 interface NumberFormatCustomProps {
-  inputRef: (instance: NumberFormat | null) => void;
+  inputRef: (instance: HTMLInputElement | null) => void;
   onChange: (event: { target: { name: string; value: string } }) => void;
   name: string;
+}
+
+type NumberFormatValuesType = {
+  formattedValue: string;
+  value: string;
+  floatValue: number|undefined;
+};
+
+function isValueAllowed(values: NumberFormatValuesType){
+  // Absolute value of Amount/Fee must be less than MAX_SAFE_INTEGER
+  const canBigIntBeConstructed = /^[0-9]+$/.test(values.value);
+  if(canBigIntBeConstructed){
+    const N = BigInt(values.value);
+    if(N > BigInt(Number.MAX_SAFE_INTEGER) || N < 0){
+      return false;
+    }
+    if(/^00/.test(values.value)){
+      return false;
+    }
+  }
+
+  // Do not allow non-numeric string. But strings like below are OK.
+  //   .  =>  This leads to decimal like .123456
+  const cannotConstructNumberAnyMore = (
+    isNaN(+values.value)
+    && !(/^\.[0-9]*$/.test(values.value))
+  );
+  if(cannotConstructNumberAnyMore){
+    return false;
+  }
+
+  // Negative number is not allowed
+  const isNegative = values.value.charAt(0) === "-";
+  if(isNegative){
+    return false;
+  }
+
+  // Check fraction part of double precision floating point
+  // e.g. The fraction part of 90071992.54740991 is 53 bit,
+  // which is beyond the specification(52 bit) so it should be an error.
+  const normalizedValue = values.value
+    // .123 => 0.123
+    .replace(/^\./, "0.")
+    // 0. => 0 ; 0.00 => 0
+    .replace(/^0\.0*$/, "0")
+    // 12. => 12
+    .replace(/([0-9])\.$/, "$1")
+    // 0.123000 => 0.123
+    .replace(/([1-9]+)0+$/, "$1")
+    // 0.000000123 => 123
+    // Because for example (0.0000001).toString() will be '1e-7' (not "0.0000001")
+    // and this exponential style stringification makes validation below not working
+    .replace(/^0\.0{6,}/, "")
+  ;
+  const fractionOverflow =
+    !canBigIntBeConstructed
+    && values.value !== ""
+    && (+normalizedValue).toString() !== normalizedValue;
+  return !fractionOverflow;
 }
 
 function NumberFormatCustom(props: NumberFormatCustomProps) {
   const { inputRef, onChange, ...other } = props;
 
-  function handleChange(values: Object) {
-    onChange(values.value);
+  function handleChange(values: NumberFormatValuesType) {
+    onChange({target: {name: props.name, value: values.value}});
   }
 
   return (
@@ -36,6 +95,7 @@ function NumberFormatCustom(props: NumberFormatCustomProps) {
       thousandSeparator
       allowNegative={false}
       isNumericString
+      isAllowed={isValueAllowed}
     />
   );
 }
@@ -62,8 +122,8 @@ export default function Amount(props: AmountProps) {
 
   const currencyCode = symbol === undefined ? defaultCurrencyCode : symbol;
   const isChiaCurrency = ['XCH', 'TXCH'].includes(currencyCode);
-  const mojo = isChiaCurrency 
-    ? chiaToMojo(correctedValue) 
+  const mojo = isChiaCurrency
+    ? chiaToMojo(correctedValue)
     : catToMojo(correctedValue);
 
   return (
