16097808988556e4822ceeced818d133f54d6e73
Jeff Cruikshank <jeff@chia.net>
2022-03-09 14:24:18 -0800

    Associate cached TransactionCount with its corresponding walletId.

--- packages/api-react/src/services/wallet.ts
+++ packages/api-react/src/services/wallet.ts
@@ -230,9 +230,9 @@ export const walletApi = apiWithTag.injectEndpoints({
         service: Wallet,
         args: [walletId],
       }),
-      invalidatesTags: [
+      invalidatesTags: (_result, _error, { walletId }) => [
         { type: 'Transactions', id: 'LIST' },
-        'TransactionCount',
+        { type: 'TransactionCount', id: walletId },
       ],
     }),
 
@@ -634,7 +634,9 @@ export const walletApi = apiWithTag.injectEndpoints({
         args: [walletId],
       }),
       transformResponse: (response: any) => response?.count,
-      providesTags: ['TransactionCount'],
+      providesTags: (result, _error, { walletId }) => result
+        ? [{ type: 'TransactionCount', id: walletId }]
+        : [],
       onCacheEntryAdded: onCacheEntryAddedInvalidate(baseQuery, [{
         command: 'onCoinAdded',
         service: Wallet,
