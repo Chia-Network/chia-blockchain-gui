7eaa6f96e9dbff8abf77f4249cfa5ef041b27bc1
Jack Nelson <jack@jacknelson.xyz>
2022-06-26 12:25:03 -0400

    Shorten string

--- packages/gui/src/components/plot/add/PlotAddNumberOfPlots.tsx
+++ packages/gui/src/components/plot/add/PlotAddNumberOfPlots.tsx
@@ -315,7 +315,7 @@ export default function PlotAddNumberOfPlots(props: Props) {
                 name="plotNFTContractAddr"
                 type="text"
                 variant="filled"
-                placeholder="Plot NFT Plot Target Address, This will be used if a PlotNFT is not selected."
+                placeholder="Plot NFT Plot Target Address"
                 label={<Trans>Plot NFT Pool Contract Address</Trans>}
               />
             </FormControl>
