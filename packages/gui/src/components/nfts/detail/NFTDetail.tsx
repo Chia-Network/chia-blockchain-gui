47eb6f18aab4de501bfcc745e9fb7fa03399905c
Jsewill <abraham.sewill@gmail.com>
2022-08-09 16:46:03 -0500

    Update NFTDetail.tsx

--- packages/gui/src/components/nfts/detail/NFTDetail.tsx
+++ packages/gui/src/components/nfts/detail/NFTDetail.tsx
@@ -73,8 +73,8 @@ export default function NFTDetail() {
                   <Trans>Description</Trans>
                 </Typography>
 
-                <Typography>
-                  {metadata?.description?.split('\n').map((line,i) => <p key={i}>{line}</p>) ?? <Trans>Not Available</Trans>}
+                <Typography sx={{ whiteSpace : 'pre-line'}}>
+                  {metadata?.description ?? <Trans>Not Available</Trans>}
                 </Typography>
               </Flex>
               {metadata?.collection?.name && (
