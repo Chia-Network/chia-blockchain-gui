aa2ed521089a3314e7347a5bcf852673d6549da4
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-08-27 14:45:11 -0500

    Update DarkModeToggle.tsx

--- packages/core/src/components/DarkModeToggle/DarkModeToggle.tsx
+++ packages/core/src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { IconButton } from '@mui/material';
 import { Brightness4, Brightness7 } from '@mui/icons-material';
 import useDarkMode from '../../hooks/useDarkMode';
+import isElectron from 'is-electron';
 
 export default function DarkModeToggle() {
   const { toggle, isDarkMode } = useDarkMode();
 
   function handleClick() {
     toggle();
+    if (isElectron()) {
+      const { nativeTheme } = window.require('@electron/remote');
+      nativeTheme.themeSource = isDarkMode ? 'dark' : 'light';
+    }
   }
 
   return (
