6f9a2455f96d49a50f9ee540e469730cdee4ebe2
ChiaMineJP <admin@chiamine.jp>
2022-10-20 02:27:45 +0900

    Reorganized plot size components

--- packages/gui/src/constants/plotSizes.ts
+++ packages/gui/src/constants/plotSizes.ts
@@ -1,3 +1,5 @@
+import PlotterName from "./PlotterName";
+
 type PlotSize = {
   label: string;
   value: number;
@@ -5,25 +7,44 @@ type PlotSize = {
   defaultRam: number;
 };
 
-export const defaultPlotSize: PlotSize = {
-  label: '101.4GiB',
-  value: 32,
-  workspace: '239GiB',
-  defaultRam: 3390,
+export function getPlotSize(kSize: 25|32|33|34|35){
+  return {
+    25: '600MiB',
+    32: '101.4GiB',
+    33: '208.8GiB',
+    34: '429.8GiB',
+    35: '884.1GiB',
+  }[kSize] || "Size Unknown";
+}
+
+export const plottingInfo: Record<PlotterName, PlotSize[]> = {
+  [PlotterName.CHIAPOS]: [
+    { value: 25, label: getPlotSize(25), workspace: '1.8GiB', defaultRam: 512 },
+    { value: 32, label: getPlotSize(32), workspace: '239GiB', defaultRam: 3390 },
+    { value: 33, label: getPlotSize(33), workspace: '521GiB', defaultRam: 7400 },
+    // workspace are guesses using 55.35% - rounded up - past here
+    { value: 34, label: getPlotSize(34), workspace: '1041GiB', defaultRam: 14800 },
+    { value: 35, label: getPlotSize(35), workspace: '2175GiB', defaultRam: 29600 },
+  ],
+  [PlotterName.MADMAX]: [
+    { value: 25, label: getPlotSize(25), workspace: '1.8GiB', defaultRam: 512 },
+    { value: 32, label: getPlotSize(32), workspace: '239GiB', defaultRam: 3390 },
+    { value: 33, label: getPlotSize(33), workspace: '521GiB', defaultRam: 7400 },
+    // workspace are guesses using 55.35% - rounded up - past here
+    { value: 34, label: getPlotSize(34), workspace: '1041GiB', defaultRam: 14800 },
+    { value: 35, label: getPlotSize(35), workspace: '2175GiB', defaultRam: 29600 },
+  ],
+  [PlotterName.BLADEBIT]: [
+    { value: 32, label: getPlotSize(32), workspace: '416GiB', defaultRam: 3390 },
+  ],
+  [PlotterName.BLADEBIT2]: [
+    { value: 32, label: getPlotSize(32), workspace: '480GiB', defaultRam: 3390 },
+  ],
 };
 
-const plotSizes: PlotSize[] = [
-  { label: '600MiB', value: 25, workspace: '1.8GiB', defaultRam: 512 },
-  defaultPlotSize,
-  { label: '208.8GiB', value: 33, workspace: '521GiB', defaultRam: 7400 },
-  // workspace are guesses using 55.35% - rounded up - past here
-  { label: '429.8GiB', value: 34, workspace: '1041GiB', defaultRam: 14800 },
-  { label: '884.1GiB', value: 35, workspace: '2175GiB', defaultRam: 29600 },
-];
-
-export const plotSizeOptions = plotSizes.map((item) => ({
-  value: item.value,
-  label: `${item.label} (k=${item.value}, temporary space: ${item.workspace})`,
-}));
-
-export default plotSizes;
+export function getPlotSizeOptions(plotterName: PlotterName){
+  return plottingInfo[plotterName].map((item) => ({
+    value: item.value,
+    label: `${item.label} (k=${item.value}, temporary space: ${item.workspace})`,
+  }));
+}
