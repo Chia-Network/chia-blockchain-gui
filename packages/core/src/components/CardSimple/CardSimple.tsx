5be1929f79867c91071fe72214b437a971825190
Zlatko Fedor <zlatko@chia.net>
2022-10-24 18:28:18 +0200

    GUI: Wallet balance strings can overflow#1040 (#1088)

--- packages/core/src/components/CardSimple/CardSimple.tsx
+++ packages/core/src/components/CardSimple/CardSimple.tsx
@@ -1,5 +1,4 @@
 import React, { type ReactNode, ReactElement } from 'react';
-import styled from 'styled-components';
 import { Trans } from '@lingui/macro';
 import Flex from '../Flex';
 import TooltipIcon from '../TooltipIcon';
@@ -11,20 +10,6 @@ import {
   TypographyProps,
   CircularProgress,
 } from '@mui/material';
-
-const StyledCard = styled(Card)`
-  height: 100%;
-  overflow: visible;
-  margin-bottom: -0.5rem;
-`;
-
-const StyledTitle = styled(Box)`
-  margin-bottom: 0.5rem;
-`;
-
-const StyledValue = styled(Typography)`
-  font-size: 1.25rem;
-`;
 
 export type CardSimpleProps = {
   title: ReactNode;
@@ -39,36 +24,58 @@ export type CardSimpleProps = {
 };
 
 export default function CardSimple(props: CardSimpleProps) {
-  const { title, value, description, valueColor, loading, tooltip, error, actions, children } = props;
+  const {
+    title,
+    value,
+    description,
+    valueColor = 'primary',
+    loading = false,
+    tooltip,
+    error,
+    actions,
+    children,
+  } = props;
 
   return (
-    <StyledCard>
-      <CardContent sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-        <StyledTitle>
-          <Flex flexGrow={1} justifyContent="space-between" alignItems="center" flexWrap="wrap" gap={0.5}>
+    <Card sx={{ height: '100%', overflow: 'visible' }}>
+      <CardContent
+        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+      >
+        <Box marginBottom={0.5}>
+          <Flex
+            flexGrow={1}
+            justifyContent="space-between"
+            alignItems="center"
+            flexWrap="wrap"
+            gap={0.5}
+          >
             <Flex gap={1} alignItems="center">
               <Typography color="textSecondary">{title}</Typography>
               {tooltip && <TooltipIcon>{tooltip}</TooltipIcon>}
             </Flex>
             {actions}
           </Flex>
-        </StyledTitle>
+        </Box>
         {loading ? (
           <Box>
             <CircularProgress color="secondary" size={25} />
           </Box>
         ) : error ? (
           <Flex alignItems="center">
-            <StyledValue variant="h5" color="error">
+            <Typography variant="h5" color="error" fontSize="1.25rem">
               <Trans>Error</Trans>
-            </StyledValue>
+            </Typography>
             &nbsp;
             <TooltipIcon>{error?.message}</TooltipIcon>
           </Flex>
         ) : (
-          <StyledValue variant="h5" color={valueColor}>
+          <Typography
+            variant="h5"
+            color={valueColor}
+            sx={{ wordWrap: 'break-word', fontSize: '1.25rem' }}
+          >
             {value}
-          </StyledValue>
+          </Typography>
         )}
         {description && (
           <Typography variant="caption" color="textSecondary" flexGrow={1}>
@@ -77,14 +84,6 @@ export default function CardSimple(props: CardSimpleProps) {
         )}
         {children}
       </CardContent>
-    </StyledCard>
+    </Card>
   );
 }
-
-CardSimple.defaultProps = {
-  valueColor: 'primary',
-  description: undefined,
-  loading: false,
-  value: undefined,
-  error: undefined,
-};
