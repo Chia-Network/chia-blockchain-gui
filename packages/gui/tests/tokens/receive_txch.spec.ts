11e18f2ac5dd7bb8c86392e5f2fa26e669f17a1d
jfawChia <105236411+jfawChia@users.noreply.github.com>
2022-10-29 00:13:37 -0400

    Feature cleanup playwright (#1117)
    * clean up of files and renaming feature
    
    * took out hardcoded lines based off Jeff comments
    
    * File changes for data-attributes added to Wallet page.
    
    * This commit contains clean up for hard coded directories, renaming features and intro test for Key Mnenomic test

--- packages/gui/tests/tokens/receive_txch.spec.ts
+++ packages/gui/tests/tokens/receive_txch.spec.ts
@@ -20,47 +20,47 @@ test.afterAll(async () => {
 
 });
 
-  test('Verify that receiving wallet receives funds from send wallet!', async () => {
+  test('Verify that a recipient wallet receives funds from sending wallet!', async () => {
     //Pre-requisites
     let receive_wallet = 'txch1u237ltq0pp4348ppwv6cge7fks87mn4wz3c0ywvgswvpwhkqqn8qn8jeq6'
     let send_wallet = 'txch160r38yfdqfeplderp7tf8zm7u8tv48vm0qd3rs3r2kp4yw4ahl8qd27qrr'
 
-    // Given I enter correct credentials in Passphrase dialog
+    //Given I enter correct credentials in Passphrase dialog
     await new LoginPage(page).login('password2022!@')
 
-    // And I navigate to a wallet with funds
+    //And I navigate to a wallet with funds
     await page.locator('[data-testid="LayoutDashboard-log-out"]').click();
     await page.locator('text=1922132445').click();
 
-    // And I check the balance of current wallet
+    //And I check the balance of current wallet
     while (!isWalletSynced('1922132445')) {
       console.log('Waiting for wallet to sync...');
       await page.waitForTimeout(1000);
     }
 
-    // And I click on Send Page
+    //And I click on Send Page
     await page.locator('[data-testid="WalletHeader-tab-send"]').click();
 
-    // When I complete the send page required fields
+    //When I complete the send page required fields
     await new SendFunds(page).send(receive_wallet, '0.01', '0.00005' )
  
     //Then I receive a success message
     await expect(page.locator('div[role="dialog"]')).toHaveText("SuccessTransaction has successfully been sent to a full node and included in the mempool.OK" );
     await page.locator('div[role="dialog"] >> text=OK').click();
 
-    // Given I navigate out to all wallets
+    //Given I navigate out to all wallets
     await page.locator('[data-testid="LayoutDashboard-log-out"]').click();
 
-    // When I navigate to a receive wallet 
+    //When I navigate to the receive wallet 
     await page.locator('text=854449615').click();
 
-    // And I navigate to Summary page 
+    //And I navigate to Summary page 
     await page.locator('[data-testid="WalletHeader-tab-summary"]').click();
 
-    // Then Transactions section display the correct wallet, amount and fee
+    //Then Transactions section display the correct wallet, amount and fee
     await expect(page.locator(`text=${receive_wallet} >> nth=0`)).toBeVisible();
 
-    // Begin: Wait for Wallet to Sync
+    //Begin: Wait for Wallet to Sync
     while (!isWalletSynced('854449615')) {
       console.log('Waiting for wallet to sync...');
       await page.waitForTimeout(1000);
@@ -75,7 +75,7 @@ test.afterAll(async () => {
     
     // Given I send funds back 
     await page.locator('[data-testid="WalletHeader-tab-send"]').click();
-    await new SendFunds(page).send(send_wallet, '0.01', '0.00005' )
+    await new SendFunds(page).send(send_wallet, '0.01', '0.000005' )
  
   });
 
