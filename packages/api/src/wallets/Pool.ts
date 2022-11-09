4c35627607634290991a3381d0c8b5ea4e40d9c5
Jeff <jeff@chia.net>
2022-11-09 12:21:15 -0800

    Remove vestiges of defunct backup service (#1164)
    * Remove vestiges of defunct backup service
    
    * Missed this in my prior commit

--- packages/api/src/wallets/Pool.ts
+++ packages/api/src/wallets/Pool.ts
@@ -1,15 +1,10 @@
 import Wallet from '../services/Wallet';
 
 export default class PoolWallet extends Wallet {
-  async createNewWallet(
-    initialTargetState: Object,
-    fee: string, 
-    host: string = this.client.backupHost,
-  ) {
+  async createNewWallet(initialTargetState: Object, fee: string) {
     return super.createNewWallet('pool_wallet', {
       mode: 'new',
       fee,
-      host,
       initialTargetState,
     });
   }
