04645748908edd1befad133a6fbf5a81f48b14d6
Zsolt Szabo <zsolt.dev@icloud.com>
2022-11-28 16:35:07 +0100

    Remove some unused files and code

--- packages/wallets/src/components/Wallets.tsx
+++ packages/wallets/src/components/Wallets.tsx
@@ -4,14 +4,10 @@ import { Navigate, Routes, Route } from 'react-router-dom';
 
 import Wallet from './Wallet';
 import WalletsSidebar from './WalletsSidebar';
-import WalletCreate from './create/WalletCreate';
 
 export default function Wallets() {
   return (
     <Routes>
-      <Route element={<LayoutDashboardSub outlet />}>
-        <Route path="create/*" element={<WalletCreate />} />
-      </Route>
       <Route element={<LayoutDashboardSub sidebar={<WalletsSidebar />} outlet />}>
         <Route path=":walletId" element={<Wallet />} />
         <Route path="*" element={<Navigate to="1" />} />
