73116323faf4085e1c60bf585bd7b9c4f0d2dc65
Jeff Cruikshank <jeff@chia.net>
2023-01-06 12:37:30 -0800

    Add a context provider to manage the NFT gallery scroll state

--- packages/gui/src/components/nfts/NFTs.tsx
+++ packages/gui/src/components/nfts/NFTs.tsx
@@ -3,14 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 
 import NFTDetail from './detail/NFTDetailV2';
 import NFTGallery from './gallery/NFTGallery';
+import NFTGalleryScrollPositionProvider from './gallery/NFTGalleryScrollPositionProvider';
 
 /* ========================================================================== */
 
 export default function NFTs() {
   return (
-    <Routes>
-      <Route index element={<NFTGallery />} />
-      <Route path=":nftId" element={<NFTDetail />} />
-    </Routes>
+    /*
+    Install NFTGalleryScrollPositionProvider to store/restore the scroll position only
+    navigating within NFT screens. This allows the user to navigate into NFT details, and
+    then back to the gallery without losing the scroll position. When navigating to other
+    non-NFT screens, and then back, the scroll position is reset to the top.
+    */
+    <NFTGalleryScrollPositionProvider>
+      <Routes>
+        <Route index element={<NFTGallery />} />
+        <Route path=":nftId" element={<NFTDetail />} />
+      </Routes>
+    </NFTGalleryScrollPositionProvider>
   );
 }
