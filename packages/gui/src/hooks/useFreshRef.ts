8c08ca61fef1d7a4b7bc731157fbec7183fc0bd7
Zsolt Szabo <zsolt.dev@icloud.com>
2023-01-11 03:29:27 +0100

    Remove unused files (#1294)

--- packages/gui/src/hooks/useFreshRef.ts
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react';
-
-export default function useFreshRef<Data>(data: Data) {
-  const ref = useRef(data);
-
-  ref.current = data;
-
-  return ref;
-}
--- packages/gui/src/types/Block.ts
+++
@@ -1,9 +0,0 @@
-import type Foliage from './Foliage';
-import type FoliageTransactionBlock from './FoliageTransactionBlock';
-
-type Block = {
-  foliage_transaction_block: FoliageTransactionBlock;
-  foliage: Foliage;
-};
-
-export default Block;
