d7cd6d01a67ba14b2e83c523ac26007f0cfd31b3
Jeff Cruikshank <jeff@chia.net>
2023-01-28 18:35:13 -0800

    fix enable auto login also removes the "Delete All Keys" option when no keys are present

--- packages/core/src/screens/SelectKey/SelectKey.tsx
+++ packages/core/src/screens/SelectKey/SelectKey.tsx
@@ -180,17 +180,19 @@ export default function SelectKey() {
           >
             <Trans>Import from Mnemonics (12 or 24 words)</Trans>
           </Button>
-          <Button
-            onClick={handleDeleteAllKeys}
-            variant="outlined"
-            color="danger"
-            size="large"
-            disabled={isLoading}
-            data-testid="SelectKey-delete-all-keys"
-            fullWidth
-          >
-            <Trans>Delete all keys</Trans>
-          </Button>
+          {hasFingerprints && (
+            <Button
+              onClick={handleDeleteAllKeys}
+              variant="outlined"
+              color="danger"
+              size="large"
+              disabled={isLoading}
+              data-testid="SelectKey-delete-all-keys"
+              fullWidth
+            >
+              <Trans>Delete all keys</Trans>
+            </Button>
+          )}
         </Flex>
       </Flex>
     </StyledContainer>
