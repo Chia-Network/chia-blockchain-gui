64ce4a2e495f06d4e6cc11f963c8e2ad1d5326a5
Jeff Cruikshank <jeff@chia.net>
2023-01-31 17:39:43 -0800

    fix wording in offer sharing summary dialog

--- packages/gui/src/components/offers/OfferShareDialog.tsx
+++ packages/gui/src/components/offers/OfferShareDialog.tsx
@@ -1310,12 +1310,12 @@ function OfferShareConfirmationDialog(props: OfferShareConfirmationDialogProps) 
             summary={offerRecord.summary}
             makerTitle={
               <Typography variant="subtitle1">
-                <Trans>Your offer:</Trans>
+                <Trans>Assets I am offering:</Trans>
               </Typography>
             }
             takerTitle={
               <Typography variant="subtitle1">
-                <Trans>In exchange for:</Trans>
+                <Trans>Assets I will receive:</Trans>
               </Typography>
             }
             rowIndentation={3}
@@ -1481,7 +1481,7 @@ export default function OfferShareDialog(props: OfferShareDialogProps) {
       </DialogTitle>
 
       <DialogContent dividers>
-        <Flex flexDirection="column" gap={2}>
+        <Flex flexDirection="column" gap={2} paddingTop="16px">
           <Flex flexDirection="column" gap={2}>
             <Typography variant="subtitle1">Where would you like to share your offer?</Typography>
             <Flex flexDirection="column" gap={3}>
