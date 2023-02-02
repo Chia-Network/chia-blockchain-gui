6cbbfda4092e9a4a4a4010e6159cfee17885dbb9
Jeff Cruikshank <jeff@chia.net>
2023-02-02 14:42:23 -0800

    launch daemon if self_hostname === ui.daemon_host

--- packages/gui/src/util/manageDaemonLifetime.ts
+++ packages/gui/src/util/manageDaemonLifetime.ts
@@ -5,9 +5,10 @@ import { readConfigFile } from './loadConfig';
 export default function manageDaemonLifetime(net?: string): boolean {
   try {
     const config = readConfigFile(net);
-    const selfHostname = get(config, 'ui.daemon_host', 'localhost');
+    const selfHostname = get(config, 'self_hostname', 'localhost');
+    const uiDaemonHost = get(config, 'ui.daemon_host', 'localhost');
 
-    return selfHostname === 'localhost';
+    return selfHostname === uiDaemonHost;
   } catch (error: any) {
     if (error.code === 'ENOENT') {
       // configuration file does not exists, use default value
