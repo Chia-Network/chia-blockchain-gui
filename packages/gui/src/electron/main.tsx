b41777358dddd731069082116bad6c6fd155034a
Zlatko Fedor <zfedor@gmail.com>
2023-02-02 23:27:34 +0100

    fixed broken loading of remote files

--- packages/gui/src/electron/main.tsx
+++ packages/gui/src/electron/main.tsx
@@ -273,7 +273,7 @@ if (!handleSquirrelEvent()) {
         });
       }
 
-      const allRequests: any = {};
+      const allRequests: Record<string, { abort: () => void; promise: Promise<any> }> = {};
 
       ipcMain.handle('abortFetchingBinary', (_event, uri: string) => {
         if (allRequests[uri]) {
@@ -312,154 +312,159 @@ if (!handleSquirrelEvent()) {
         async (_event, requestOptions = {}, requestHeaders = {}, requestData?: any) => {
           const { maxSize = Infinity, ...rest } = requestOptions;
 
-          let wasCached = false;
-
           if (allRequests[rest.url]) {
-            /* request already exists */
-            return undefined;
+            return allRequests[rest.url].promise;
           }
 
-          allRequests[rest.url] = net.request(rest);
+          const request = net.request(rest);
 
-          const nftIdUrl = `${rest.nftId}_${rest.url}`;
-          const fileOnDisk = path.join(thumbCacheFolder, computeHash(nftIdUrl, { encoding: 'utf-8' }));
+          allRequests[rest.url] = {
+            abort: () => request.abort(),
+            promise: new Promise(async (resolveResult) => {
+              let wasCached = false;
+              let error: Error | undefined;
+              let statusCode: number | undefined;
+              let statusMessage: string | undefined;
+              let contentType: string | undefined;
+              let encoding = 'binary';
+              let dataObject: { isValid?: boolean; content: string } = {
+                content: '',
+              };
 
-          const fileStream = fs.createWriteStream(fileOnDisk);
+              const nftIdUrl = `${rest.nftId}_${rest.url}`;
+              const fileOnDisk = path.join(thumbCacheFolder, computeHash(nftIdUrl, { encoding: 'utf-8' }));
 
-          Object.entries(requestHeaders).forEach(([header, value]: [string, any]) => {
-            allRequests[rest.url].setHeader(header, value);
-          });
+              try {
+                const fileStream = fs.createWriteStream(fileOnDisk);
 
-          let error: Error | undefined;
-          let statusCode: number | undefined;
-          let statusMessage: string | undefined;
-          let contentType: string | undefined;
-          let encoding = 'binary';
-          let dataObject: { isValid?: boolean; content: string } = {
-            content: '',
-          };
+                Object.entries(requestHeaders).forEach(([header, value]: [string, any]) => {
+                  request.setHeader(header, value);
+                });
 
-          const buffers: Buffer[] = [];
-          let totalLength = 0;
+                const buffers: Buffer[] = [];
+                let totalLength = 0;
 
-          try {
-            /* GET FILE SIZE */
-            const fileSize: number = await getRemoteFileSize(rest.url);
-            dataObject = await new Promise((resolve, reject) => {
-              allRequests[rest.url].on('response', (response: IncomingMessage) => {
-                statusCode = response.statusCode;
-                statusMessage = response.statusMessage;
+                /* GET FILE SIZE */
+                const fileSize: number = await getRemoteFileSize(rest.url);
+                dataObject = await new Promise((resolve, reject) => {
+                  request.on('response', (response: IncomingMessage) => {
+                    statusCode = response.statusCode;
+                    statusMessage = response.statusMessage;
 
-                const rawContentType = response.headers['content-type'];
-                if (rawContentType) {
-                  if (Array.isArray(rawContentType)) {
-                    [contentType] = rawContentType;
-                  } else {
-                    contentType = rawContentType;
-                  }
+                    const rawContentType = response.headers['content-type'];
+                    if (rawContentType) {
+                      if (Array.isArray(rawContentType)) {
+                        [contentType] = rawContentType;
+                      } else {
+                        contentType = rawContentType;
+                      }
 
-                  if (contentType) {
-                    // extract charset from contentType
-                    const charsetMatch = contentType.match(/charset=([^;]+)/);
-                    if (charsetMatch) {
-                      [, encoding] = charsetMatch;
+                      if (contentType) {
+                        // extract charset from contentType
+                        const charsetMatch = contentType.match(/charset=([^;]+)/);
+                        if (charsetMatch) {
+                          [, encoding] = charsetMatch;
+                        }
+                      }
                     }
-                  }
-                }
 
-                response.on('data', (chunk) => {
-                  buffers.push(chunk);
+                    response.on('data', (chunk) => {
+                      buffers.push(chunk);
 
-                  fileStream.write(chunk);
+                      fileStream.write(chunk);
 
-                  totalLength += chunk.byteLength;
+                      totalLength += chunk.byteLength;
 
-                  if (fileSize > 0) {
-                    mainWindow?.webContents.send('fetchBinaryContentProgress', {
-                      nftIdUrl,
-                      progress: totalLength / fileSize,
+                      if (fileSize > 0) {
+                        mainWindow?.webContents.send('fetchBinaryContentProgress', {
+                          nftIdUrl,
+                          progress: totalLength / fileSize,
+                        });
+                      }
+                      if (totalLength > maxSize || fileSize > maxSize) {
+                        if (request) {
+                          request.abort();
+                          if (fs.existsSync(fileOnDisk)) {
+                            fs.unlinkSync(fileOnDisk);
+                          }
+                        }
+                        reject(new Error('Response too large'));
+                      }
                     });
-                  }
-                  if (totalLength > maxSize || fileSize > maxSize) {
-                    if (allRequests[rest.url]) {
-                      allRequests[rest.url].abort();
-                      if (fs.existsSync(fileOnDisk)) {
-                        fs.unlinkSync(fileOnDisk);
-                      }
-                    }
-                    reject(new Error('Response too large'));
-                  }
-                });
 
-                response.on('end', () => {
-                  let content;
-                  // special case for iso-8859-1, which is mapped to 'latin1' in node
-                  if (encoding.toLowerCase() === 'iso-8859-1') {
-                    encoding = 'latin1';
-                  }
-                  try {
-                    content = Buffer.concat(buffers).toString(encoding as BufferEncoding);
-                  } catch (e: any) {
-                    console.error(`Failed to convert data to string using encoding ${encoding}: ${e.message}`);
-                  }
-                  fileStream.end();
-                  getChecksum(fileOnDisk).then((checksum) => {
-                    const isValid = (checksum as string).replace(/^0x/, '') === rest.dataHash.replace(/^0x/, '');
-                    if (rest.forceCache) {
-                      /* should we cache it or delete it? */
-                      if (shouldCacheFile(fileOnDisk)) {
-                        wasCached = true;
-                      } else if (fs.existsSync(fileOnDisk)) {
-                        fs.unlinkSync(fileOnDisk);
+                    response.on('end', () => {
+                      let content;
+                      // special case for iso-8859-1, which is mapped to 'latin1' in node
+                      if (encoding.toLowerCase() === 'iso-8859-1') {
+                        encoding = 'latin1';
                       }
-                      mainWindow?.webContents.send('fetchBinaryContentDone', {
-                        nftIdUrl,
-                        valid: isValid,
+                      try {
+                        content = Buffer.concat(buffers).toString(encoding as BufferEncoding);
+                      } catch (e: any) {
+                        console.error(`Failed to convert data to string using encoding ${encoding}: ${e.message}`);
+                      }
+                      fileStream.end();
+                      getChecksum(fileOnDisk).then((checksum) => {
+                        const isValid = (checksum as string).replace(/^0x/, '') === rest.dataHash.replace(/^0x/, '');
+                        if (rest.forceCache) {
+                          /* should we cache it or delete it? */
+                          if (shouldCacheFile(fileOnDisk)) {
+                            wasCached = true;
+                          } else if (fs.existsSync(fileOnDisk)) {
+                            fs.unlinkSync(fileOnDisk);
+                          }
+                          mainWindow?.webContents.send('fetchBinaryContentDone', {
+                            nftIdUrl,
+                            valid: isValid,
+                          });
+                          const extension = parseExtensionFromUrl(rest.url);
+                          resolve({
+                            isValid,
+                            content: extension === 'svg' ? content : '',
+                          });
+                        } else {
+                          resolve({ isValid, content });
+                        }
                       });
-                      const extension = parseExtensionFromUrl(rest.url);
-                      resolve({
-                        isValid,
-                        content: extension === 'svg' ? content : '',
-                      });
-                    } else {
-                      resolve({ isValid, content });
-                    }
+                      delete allRequests[rest.url];
+                    });
+
+                    response.on('error', (e: string) => {
+                      fileStream.end();
+                      reject(new Error(e));
+                    });
                   });
-                  delete allRequests[rest.url];
+
+                  request.on('error', (err: any) => {
+                    reject(err);
+                  });
+
+                  if (requestData) {
+                    request.write(requestData);
+                  }
+
+                  request.end();
                 });
-
-                response.on('error', (e: string) => {
-                  fileStream.end();
-                  reject(new Error(e));
-                });
-              });
-
-              allRequests[rest.url].on('error', (err: any) => {
-                reject(err);
-              });
-
-              if (requestData) {
-                allRequests[rest.url].write(requestData);
+              } catch (e: any) {
+                if (fs.existsSync(fileOnDisk)) {
+                  fs.unlinkSync(fileOnDisk);
+                }
+                delete allRequests[rest.url];
+                error = e;
               }
 
-              allRequests[rest.url].end();
-            });
-          } catch (e: any) {
-            if (fs.existsSync(fileOnDisk)) {
-              fs.unlinkSync(fileOnDisk);
-            }
-            delete allRequests[rest.url];
-            error = e;
-          }
-
-          return {
-            error,
-            statusCode,
-            statusMessage,
-            encoding,
-            dataObject,
-            wasCached,
+              resolveResult({
+                error,
+                statusCode,
+                statusMessage,
+                encoding,
+                dataObject,
+                wasCached,
+              });
+            }),
           };
+
+          return allRequests[rest.url].promise;
         }
       );
 
