5a1c7c64107d25f9d94849fa83f19e060cca7a7f
Jeff Cruikshank <jeff@chia.net>
2023-02-17 14:23:52 -0800

    displayComponent now receives the full list of params WalletConnectCommandParam name now references an enum

--- packages/gui/src/@types/WalletConnectCommandParam.ts
+++ packages/gui/src/@types/WalletConnectCommandParam.ts
@@ -1,14 +1,16 @@
 import type BigNumber from 'bignumber.js';
 import { type ReactNode } from 'react';
 
+import WalletConnectCommandParamName from './WalletConnectCommandParamName';
+
 type WalletConnectCommandParam = {
-  name: string;
+  name: WalletConnectCommandParamName;
   isOptional?: boolean;
   label?: ReactNode;
   description?: ReactNode;
   type?: 'string' | 'number' | 'boolean' | 'BigNumber' | 'object';
   defaultValue?: string | number | boolean | BigNumber | Record<any, any>;
-  displayComponent?: (value: any) => ReactNode;
+  displayComponent?: (value: any, params: WalletConnectCommandParam[]) => ReactNode;
   hide?: boolean;
 };
 
