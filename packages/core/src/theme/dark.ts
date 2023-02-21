71969e10f548450db70914caf75a64801212c767
Lipa Long <lipa@chia.net>
2023-02-21 09:51:36 -0500

    add colors

--- packages/core/src/theme/dark.ts
+++ packages/core/src/theme/dark.ts
@@ -21,6 +21,114 @@ export default createTheme({
       ...theme.palette.info,
       main: '#fff',
     },
+    royal: {
+      main: '#DCE6FD',
+    },
+    royalAccent: {
+      main: '#7EA9F8',
+    },
+    grape: {
+      main: '#E6E7F9',
+    },
+    grapeAccent: {
+      main: '#C3C3EE',
+    },
+    purple: {
+      main: '#F9E8FF',
+    },
+    purpleAccent: {
+      main: '#EAACFB',
+    },
+    red: {
+      main: '#FEE5E7',
+    },
+    redAccent: {
+      main: '#FAA7B0',
+    },
+    orange: {
+      main: '#FFEAC6',
+    },
+    orangeAccent: {
+      main: '#FFAF3A',
+    },
+    yellow: {
+      main: '#FEFFBD',
+    },
+    yellowAccent: {
+      main: '#FFF544',
+    },
+    lime: {
+      main: '#F1FFC7',
+    },
+    limeAccent: {
+      main: '#D4FF72',
+    },
+    green: {
+      main: '#C2F0C9',
+    },
+    greenAccent: {
+      main: '#5ECE71',
+    },
+    aqua: {
+      main: '#D1F6F2',
+    },
+    aquaAccent: {
+      main: '#6CDCD6',
+    },
+    blue: {
+      main: '#DFF2FF',
+    },
+    blueAccent: {
+      main: '#77D4FF',
+    },
+    comet: {
+      main: '#ECEEF2',
+    },
+    cometAccent: {
+      main: '#AEB9CB',
+    },
+    storm: {
+      main: '#ECECF2',
+    },
+    stormAccent: {
+      main: '#B1B2C8',
+    },
+    wine: {
+      main: '#F1ECF2',
+    },
+    wineAccent: {
+      main: '#C3B2C7',
+    },
+    sand: {
+      main: '#F0EBE4',
+    },
+    sandAccent: {
+      main: '#CCB9A5',
+    },
+    husk: {
+      main: '#F2F2E2',
+    },
+    huskAccent: {
+      main: '#D2CE9F',
+    },
+    bean: {
+      main: '#E9EBDC',
+    },
+    beanAccent: {
+      main: '#BCC294',
+    },
+    forest: {
+      main: '#E4E9E2',
+    },
+    forestAccent: {
+      main: '#9BAE98',
+    },
+    sea: {
+      main: '#DDEAE7',
+    },
+    seaAccent: {
+      main: '#9FC0BC',
+    },
     mode: 'dark',
   },
 });
--- packages/core/src/theme/light.ts
+++ packages/core/src/theme/light.ts
@@ -10,5 +10,113 @@ export default createTheme({
       ...theme.palette.background,
       card: '#fff',
     },
+    royal: {
+      main: '#C1D4FC',
+    },
+    royalAccent: {
+      main: '#6595F5',
+    },
+    grape: {
+      main: '#D2D3F3',
+    },
+    grapeAccent: {
+      main: '#9E99E0',
+    },
+    purple: {
+      main: '#F2D0FE',
+    },
+    purpleAccent: {
+      main: '#DC6EF7',
+    },
+    red: {
+      main: '#FCCFD3',
+    },
+    redAccent: {
+      main: '#F56073',
+    },
+    orange: {
+      main: '#FFD388',
+    },
+    orangeAccent: {
+      main: '#FF9B20',
+    },
+    yellow: {
+      main: '#FFFD88',
+    },
+    yellowAccent: {
+      main: '#EECC04',
+    },
+    lime: {
+      main: '#D4FF72',
+    },
+    limeAccent: {
+      main: '#94DD05',
+    },
+    green: {
+      main: '#C2F0C9',
+    },
+    greenAccent: {
+      main: '#5ECE71',
+    },
+    aqua: {
+      main: '#A2EDE6',
+    },
+    aquaAccent: {
+      main: '#3EC3C1',
+    },
+    blue: {
+      main: '#B7E6FF',
+    },
+    blueAccent: {
+      main: '#2FBEFF',
+    },
+    comet: {
+      main: '#D4D9E3',
+    },
+    cometAccent: {
+      main: '#8292AE',
+    },
+    storm: {
+      main: '#D5D5E2',
+    },
+    stormAccent: {
+      main: '#8789A9',
+    },
+    wine: {
+      main: '#DFD6E1',
+    },
+    wineAccent: {
+      main: '#A188A8',
+    },
+    sand: {
+      main: '#E0D5C8',
+    },
+    sandAccent: {
+      main: '#B19176',
+    },
+    husk: {
+      main: '#E4E2C4',
+    },
+    huskAccent: {
+      main: '#C3BA7F',
+    },
+    bean: {
+      main: '#D7DABC',
+    },
+    beanAccent: {
+      main: '#A1AA71',
+    },
+    forest: {
+      main: '#C9D3C7',
+    },
+    forestAccent: {
+      main: '#799176',
+    },
+    sea: {
+      main: '#BBD4D0',
+    },
+    seaAccent: {
+      main: '#6A9793',
+    },
   },
 });
