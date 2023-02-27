a467053c04887d9af43ec12e8409bc302c4c9d09
Lipa Long <lipa@chia.net>
2023-02-27 13:16:27 -0500

    add dark color variants

--- packages/core/src/theme/dark.ts
+++ packages/core/src/theme/dark.ts
@@ -27,11 +27,17 @@ export default createTheme({
     royalAccent: {
       main: '#7EA9F8',
     },
+    royalDark: {
+      main: '#2333AA',
+    },
     grape: {
       main: '#E6E7F9',
     },
     grapeAccent: {
       main: '#C3C3EE',
+    },
+    grapeDark: {
+      main: '#57478C',
     },
     purple: {
       main: '#F9E8FF',
@@ -39,11 +45,17 @@ export default createTheme({
     purpleAccent: {
       main: '#EAACFB',
     },
+    purpleDark: {
+      main: '#7E1A8E',
+    },
     red: {
       main: '#FEE5E7',
     },
     redAccent: {
       main: '#FAA7B0',
+    },
+    redDark: {
+      main: '#9A1739',
     },
     orange: {
       main: '#FFEAC6',
@@ -51,11 +63,17 @@ export default createTheme({
     orangeAccent: {
       main: '#FFAF3A',
     },
+    orangeDark: {
+      main: '#94280C',
+    },
     yellow: {
       main: '#FEFFBD',
     },
     yellowAccent: {
       main: '#FFF544',
+    },
+    yellowDark: {
+      main: '#87590C',
     },
     lime: {
       main: '#F1FFC7',
@@ -63,11 +81,17 @@ export default createTheme({
     limeAccent: {
       main: '#D4FF72',
     },
+    limeDark: {
+      main: '#46690B',
+    },
     green: {
       main: '#C2F0C9',
     },
     greenAccent: {
       main: '#5ECE71',
+    },
+    greenDark: {
+      main: '#1B4C24',
     },
     aqua: {
       main: '#D1F6F2',
@@ -75,11 +99,17 @@ export default createTheme({
     aquaAccent: {
       main: '#6CDCD6',
     },
+    aquaDark: {
+      main: '#195356',
+    },
     blue: {
       main: '#DFF2FF',
     },
     blueAccent: {
       main: '#77D4FF',
+    },
+    blueDark: {
+      main: '#015A8B',
     },
     comet: {
       main: '#ECEEF2',
@@ -87,11 +117,17 @@ export default createTheme({
     cometAccent: {
       main: '#AEB9CB',
     },
+    cometDark: {
+      main: '#384154',
+    },
     storm: {
       main: '#ECECF2',
     },
     stormAccent: {
       main: '#B1B2C8',
+    },
+    stormDark: {
+      main: '#3B3B51',
     },
     wine: {
       main: '#F1ECF2',
@@ -99,11 +135,17 @@ export default createTheme({
     wineAccent: {
       main: '#C3B2C7',
     },
+    wineDark: {
+      main: '#4E3B51',
+    },
     sand: {
       main: '#F0EBE4',
     },
     sandAccent: {
       main: '#CCB9A5',
+    },
+    sandDark: {
+      main: '#694C43',
     },
     husk: {
       main: '#F2F2E2',
@@ -111,11 +153,17 @@ export default createTheme({
     huskAccent: {
       main: '#D2CE9F',
     },
+    huskDark: {
+      main: '#705F3C',
+    },
     bean: {
       main: '#E9EBDC',
     },
     beanAccent: {
       main: '#BCC294',
+    },
+    beanDark: {
+      main: '#42472D',
     },
     forest: {
       main: '#E4E9E2',
@@ -123,11 +171,17 @@ export default createTheme({
     forestAccent: {
       main: '#9BAE98',
     },
+    forestDark: {
+      main: '#2B3A2B',
+    },
     sea: {
       main: '#DDEAE7',
     },
     seaAccent: {
       main: '#9FC0BC',
+    },
+    seaDark: {
+      main: '#2D4242',
     },
     mode: 'dark',
   },
--- packages/core/src/theme/light.ts
+++ packages/core/src/theme/light.ts
@@ -16,11 +16,17 @@ export default createTheme({
     royalAccent: {
       main: '#6595F5',
     },
+    royalDark: {
+      main: '#223086',
+    },
     grape: {
       main: '#D2D3F3',
     },
     grapeAccent: {
       main: '#9E99E0',
+    },
+    grapeDark: {
+      main: '#493F70',
     },
     purple: {
       main: '#F2D0FE',
@@ -28,11 +34,17 @@ export default createTheme({
     purpleAccent: {
       main: '#DC6EF7',
     },
+    purpleDark: {
+      main: '#691B74',
+    },
     red: {
       main: '#FCCFD3',
     },
     redAccent: {
       main: '#F56073',
+    },
+    redDark: {
+      main: '#841737',
     },
     orange: {
       main: '#FFD388',
@@ -40,11 +52,17 @@ export default createTheme({
     orangeAccent: {
       main: '#FF9B20',
     },
+    orangeDark: {
+      main: '#7A220D',
+    },
     yellow: {
       main: '#FFFD88',
     },
     yellowAccent: {
       main: '#EECC04',
+    },
+    yellowDark: {
+      main: '#734910',
     },
     lime: {
       main: '#D4FF72',
@@ -52,11 +70,17 @@ export default createTheme({
     limeAccent: {
       main: '#94DD05',
     },
+    limeDark: {
+      main: '#3B590E',
+    },
     green: {
       main: '#C2F0C9',
     },
     greenAccent: {
       main: '#5ECE71',
+    },
+    greenDark: {
+      main: '#1B4C24',
     },
     aqua: {
       main: '#A2EDE6',
@@ -64,11 +88,17 @@ export default createTheme({
     aquaAccent: {
       main: '#3EC3C1',
     },
+    aquaDark: {
+      main: '#194648',
+    },
     blue: {
       main: '#B7E6FF',
     },
     blueAccent: {
       main: '#2FBEFF',
+    },
+    blueDark: {
+      main: '#084A72',
     },
     comet: {
       main: '#D4D9E3',
@@ -76,11 +106,17 @@ export default createTheme({
     cometAccent: {
       main: '#8292AE',
     },
+    cometDark: {
+      main: '#323948',
+    },
     storm: {
       main: '#D5D5E2',
     },
     stormAccent: {
       main: '#8789A9',
+    },
+    stormDark: {
+      main: '#353446',
     },
     wine: {
       main: '#DFD6E1',
@@ -88,11 +124,17 @@ export default createTheme({
     wineAccent: {
       main: '#A188A8',
     },
+    wineDark: {
+      main: '#433545',
+    },
     sand: {
       main: '#E0D5C8',
     },
     sandAccent: {
       main: '#B19176',
+    },
+    sandDark: {
+      main: '#564038',
     },
     husk: {
       main: '#E4E2C4',
@@ -100,11 +142,17 @@ export default createTheme({
     huskAccent: {
       main: '#C3BA7F',
     },
+    huskDark: {
+      main: '#5B4E33',
+    },
     bean: {
       main: '#D7DABC',
     },
     beanAccent: {
       main: '#A1AA71',
+    },
+    beanDark: {
+      main: '#393E29',
     },
     forest: {
       main: '#C9D3C7',
@@ -112,11 +160,17 @@ export default createTheme({
     forestAccent: {
       main: '#799176',
     },
+    forestDark: {
+      main: '#243024',
+    },
     sea: {
       main: '#BBD4D0',
     },
     seaAccent: {
       main: '#6A9793',
+    },
+    seaDark: {
+      main: '#293838',
     },
   },
 });
