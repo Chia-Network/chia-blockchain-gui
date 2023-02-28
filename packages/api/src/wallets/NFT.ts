22a50acba87e09852d34521d2f6e60ddc6255ecd
Zsolt Szabo <zsolt.dev@icloud.com>
2023-02-28 11:16:21 +0100

    Refactor all wallet method parameters to accept object (api)

--- packages/api/src/wallets/NFT.ts
+++ packages/api/src/wallets/NFT.ts
@@ -1,86 +1,105 @@
-import { CalculateRoyaltiesRequest } from '../@types';
+import type BigNumber from 'bignumber.js';
+
+import type CalculateRoyaltiesRequest from '../@types/CalculateRoyaltiesRequest';
+import type NFTInfo from '../@types/NFTInfo';
+import type SpendBundle from '../@types/SpendBundle';
 import Wallet from '../services/WalletService';
 
 export default class NFTWallet extends Wallet {
-  async getNftsCount(walletId: number) {
-    return this.command('nft_count_nfts', {
-      walletId,
-    });
+  async getNftsCount(args: { walletId: number }) {
+    return this.command('nft_count_nfts', args);
   }
 
-  async getNfts(walletId: number, num: number, startIndex: number) {
+  async getNfts(args: { walletId: number; num: number; startIndex: number }) {
     return this.command('nft_get_nfts', {
-      walletId,
-      startIndex,
-      num: num ?? 100_000,
+      ...args,
+      num: args.num ?? 100_000,
     });
   }
 
-  async getNftInfo(coinId: string) {
-    return this.command('nft_get_info', {
-      coinId,
-    });
+  async getNftInfo(args: { coinId: string }) {
+    return this.command<{
+      nftInfo: NFTInfo;
+    }>('nft_get_info', args);
   }
 
   async getNftWalletsWithDids() {
-    return this.command('nft_get_wallets_with_dids');
+    return this.command<{
+      nftWallets: {
+        walletId: number;
+        didId: string;
+        didWalletId: number;
+      }[];
+    }>('nft_get_wallets_with_dids');
   }
 
-  async getNftWalletDid(walletId: number) {
-    return this.command('nft_get_wallet_did', {
-      walletId,
-    });
+  async getNftWalletDid(args: { walletId: number }) {
+    return this.command<{
+      didId: string;
+    }>('nft_get_wallet_did', args);
   }
 
-  async transferNft(walletId: number, nftCoinIds: string[], targetAddress: string, fee: string) {
+  async transferNft(args: { walletId: number; nftCoinIds: string[]; targetAddress: string; fee: string }) {
+    const { walletId, nftCoinIds, targetAddress, fee } = args;
     if (nftCoinIds.length === 1) {
-      return this.command('nft_transfer_nft', {
+      return this.command<{
+        walletId: number;
+        spendBundle: SpendBundle;
+      }>('nft_transfer_nft', {
         walletId,
         nftCoinId: nftCoinIds[0],
         targetAddress,
         fee,
       });
     }
-    return this.command('nft_transfer_bulk', {
+    return this.command<{
+      walletId: number[];
+      spendBundle: SpendBundle;
+      txNum: number;
+    }>('nft_transfer_bulk', {
       nftCoinList: nftCoinIds.map((nftId: string) => ({ nft_coin_id: nftId, wallet_id: walletId })),
       targetAddress,
       fee,
     });
   }
 
-  async setNftDid(walletId: number, nftCoinIds: string[], did: string, fee: string) {
+  async setNftDid(args: { walletId: number; nftCoinIds: string[]; did: string; fee: string }) {
+    const { walletId, nftCoinIds, did, fee } = args;
     if (nftCoinIds.length === 1) {
-      return this.command('nft_set_nft_did', {
+      return this.command<{
+        walletId: number;
+        spendBundle: SpendBundle;
+      }>('nft_set_nft_did', {
         walletId,
         nftCoinId: nftCoinIds[0],
         didId: did,
         fee,
       });
     }
-    return this.command('nft_set_did_bulk', {
+    return this.command<{
+      walletId: number[];
+      spendBundle: SpendBundle;
+      txNum: number;
+    }>('nft_set_did_bulk', {
       nftCoinList: nftCoinIds.map((nftId: string) => ({ nft_coin_id: nftId, wallet_id: walletId })),
       didId: did,
       fee,
     });
   }
 
-  async setNftStatus(walletId: number, nftCoinId: string, inTransaction: boolean) {
-    return this.command('nft_set_nft_status', {
+  async setNftStatus(args: { walletId: number; nftCoinId: string; inTransaction: boolean }) {
+    const { walletId, nftCoinId, inTransaction } = args;
+    return this.command<void>('nft_set_nft_status', {
       walletId,
       coinId: nftCoinId,
       inTransaction,
     });
   }
 
-  async receiveNft(walletId: number, spendBundle: any, fee: number) {
-    return this.command('nft_receive_nft', {
-      walletId,
-      spendBundle,
-      fee,
-    });
-  }
-
-  async calculateRoyalties(req: CalculateRoyaltiesRequest): Promise<Record<string, any>> {
-    return this.command('nft_calculate_royalties', req);
+  async calculateRoyalties(args: CalculateRoyaltiesRequest) {
+    return this.command<Record<string, Array<{ asset: string; address: string; amount: BigNumber | number }>>>(
+      'nft_calculate_royalties',
+      args
+    );
   }
 }
