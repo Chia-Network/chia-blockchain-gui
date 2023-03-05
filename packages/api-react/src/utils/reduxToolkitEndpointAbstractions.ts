bca7537e09c747071f47b735d17497c2f377c2e0
Zlatko Fedor <zfedor@gmail.com>
2023-03-05 21:12:35 +0100

    fixed query and mutation after transform data

--- packages/api-react/src/utils/reduxToolkitEndpointAbstractions.ts
+++ packages/api-react/src/utils/reduxToolkitEndpointAbstractions.ts
@@ -4,30 +4,44 @@ import type { BaseQueryFn } from '@reduxjs/toolkit/query/react';
 import type MethodFirstParameter from '../@types/MethodFirstParameter';
 import type MethodReturnType from '../@types/MethodReturnType';
 
+// after merge https://github.com/reduxjs/redux-toolkit/pull/2953 use this instead
+
 export function query<
   TagTypes extends string,
   ReducerPath extends string,
   Builder extends EndpointBuilder<BaseQueryFn, TagTypes, ReducerPath>,
   TClass extends new (...args: any) => any,
-  Method extends keyof InstanceType<TClass> & string
+  Method extends keyof InstanceType<TClass> & string,
+  Transform extends (response: MethodReturnType<TClass, Method>) => any
 >(
   build: Builder,
   service: TClass,
   command: Method,
   options: {
-    transformResponse?: (response: MethodReturnType<TClass, Method>) => any;
+    transformResponse?: Transform;
     onCacheEntryAdded?: Parameters<
-      typeof build.query<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>
+      typeof build.query<
+        Transform extends undefined ? MethodReturnType<TClass, Method> : ReturnType<Transform>,
+        MethodFirstParameter<TClass, Method>
+      >
     >[0]['onCacheEntryAdded'];
     providesTags?: Parameters<
-      typeof build.query<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>
+      typeof build.query<
+        Transform extends undefined ? MethodReturnType<TClass, Method> : ReturnType<Transform>,
+        MethodFirstParameter<TClass, Method>
+      >
     >[0]['providesTags'];
     invalidatesTags?: Parameters<
-      typeof build.query<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>
+      typeof build.query<
+        Transform extends undefined ? MethodReturnType<TClass, Method> : ReturnType<Transform>,
+        MethodFirstParameter<TClass, Method>
+      >
     >[0]['invalidatesTags'];
-  } = {} // Omit<Parameters<typeof build.query<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>>[0], 'query'> = {}
+  } = {}
 ) {
-  return build.query<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>({
+  type Response = Transform extends undefined ? MethodReturnType<TClass, Method> : ReturnType<Transform>;
+
+  return build.query<Response, MethodFirstParameter<TClass, Method>>({
     ...options,
     query: (args) => ({
       service,
@@ -42,25 +56,36 @@ export function mutation<
   ReducerPath extends string,
   Builder extends EndpointBuilder<BaseQueryFn, TagTypes, ReducerPath>,
   TClass extends new (...args: any) => any,
-  Method extends keyof InstanceType<TClass> & string
+  Method extends keyof InstanceType<TClass> & string,
+  Transform extends (response: MethodReturnType<TClass, Method>) => any
 >(
   build: Builder,
   service: TClass,
   command: Method,
   options: {
-    transformResponse?: (response: MethodReturnType<TClass, Method>) => any;
+    transformResponse?: Transform;
     onCacheEntryAdded?: Parameters<
-      typeof build.mutation<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>
+      typeof build.mutation<
+        Transform extends undefined ? MethodReturnType<TClass, Method> : ReturnType<Transform>,
+        MethodFirstParameter<TClass, Method>
+      >
     >[0]['onCacheEntryAdded'];
     providesTags?: Parameters<
-      typeof build.mutation<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>
+      typeof build.mutation<
+        Transform extends undefined ? MethodReturnType<TClass, Method> : ReturnType<Transform>,
+        MethodFirstParameter<TClass, Method>
+      >
     >[0]['providesTags'];
     invalidatesTags?: Parameters<
-      typeof build.mutation<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>
+      typeof build.mutation<
+        Transform extends undefined ? MethodReturnType<TClass, Method> : ReturnType<Transform>,
+        MethodFirstParameter<TClass, Method>
+      >
     >[0]['invalidatesTags'];
   } = {} // Omit<Parameters<typeof build.mutation<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>>[0], 'query'> = {}
 ) {
-  return build.mutation<MethodReturnType<TClass, Method>, MethodFirstParameter<TClass, Method>>({
+  type Response = Transform extends undefined ? MethodReturnType<TClass, Method> : ReturnType<Transform>;
+  return build.mutation<Response, MethodFirstParameter<TClass, Method>>({
     ...options,
     query: (args) => ({
       service,
