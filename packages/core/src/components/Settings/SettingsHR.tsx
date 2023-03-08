0bdc6f2c4e3f113add90e3e717a985e569d666af
Jeff <jeff@chia.net>
2023-03-07 18:12:42 -0800

    update profile settings width and other small changes (#1537)

--- packages/core/src/components/Settings/SettingsHR.tsx
+++ packages/core/src/components/Settings/SettingsHR.tsx
@@ -1,11 +1,18 @@
+import { useTheme } from '@mui/material/styles';
 import React from 'react';
 
+import useDarkMode from '../../hooks/useDarkMode';
+
 export default function SettingsHR() {
+  const theme = useTheme();
+  const { isDarkMode } = useDarkMode();
+  const backgroundColor = isDarkMode ? (theme.palette as any).border.dark : (theme.palette as any).border.main;
+
   return (
     <div>
       <hr
         style={{
-          backgroundColor: '#E0E0E0',
+          backgroundColor,
           height: 1,
           border: 0,
         }}
