ac42c28d4f8011db9b8e2789d1206a62a81b1e7e
Zsolt Szabo <zsolt.dev@icloud.com>
2023-04-07 13:18:45 +0200

    Memoize handleLogout

--- packages/api-react/src/hooks/useLogout.ts
+++ packages/api-react/src/hooks/useLogout.ts
@@ -1,12 +1,12 @@
+import { useCallback } from 'react';
+
 import { walletApi } from '../services/wallet';
 import { useAppDispatch } from '../store';
 
 export default function useLogout() {
   const dispatch = useAppDispatch();
 
-  async function handleLogout() {
-    return dispatch(walletApi.util.resetApiState());
-  }
+  const handleLogout = useCallback(async () => dispatch(walletApi.util.resetApiState()), [dispatch]);
 
   return handleLogout;
 }
