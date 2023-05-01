d923efef3a3b5a7b4c04a8a4173b4923300e687b
Lipa Long <lipa@chia.net>
2023-05-01 10:14:08 -0400

    add color value constants

--- packages/core/src/constants/index.ts
+++ packages/core/src/constants/index.ts
@@ -1,3 +1,4 @@
+export { default as ColorValues } from './ColorValues';
 export { default as Mode } from './Mode';
 export { default as State } from './State';
 export { default as StateColor } from './StateColor';
