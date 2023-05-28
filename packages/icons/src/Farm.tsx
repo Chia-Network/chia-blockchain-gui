b33a35181e83a4ffd47e8e8cdfc96f0ec9b1b8b9
ChiaMineJP <admin@chiamine.jp>
2023-05-28 17:37:34 +0900

    Updated farm icon

--- packages/icons/src/Farm.tsx
+++ packages/icons/src/Farm.tsx
@@ -4,5 +4,5 @@ import React from 'react';
 import FarmIcon from './images/Farm.svg';
 
 export default function Farm(props: SvgIconProps) {
-  return <SvgIcon component={FarmIcon} viewBox="0 0 32 37" {...props} />;
+  return <SvgIcon component={FarmIcon} viewBox="0 0 40 40" {...props} />;
 }
