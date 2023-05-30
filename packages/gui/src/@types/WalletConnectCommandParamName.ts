a57c91d0f6318c5447fb023bc348d67b77730ace
Zsolt Szabo <zsolt.dev@icloud.com>
2023-05-30 13:14:27 +0200

    Add clawback to wallet connect

--- packages/gui/src/@types/WalletConnectCommandParamName.ts
+++ packages/gui/src/@types/WalletConnectCommandParamName.ts
@@ -50,10 +50,10 @@ enum WalletConnectCommandParamName {
   TRANSACTION_ID = 'transactionId',
   URIS = 'uris',
   VALIDATE_ONLY = 'validateOnly',
-  WAIT_FOR_CONFIRMATION = 'waitForConfirmation',
   WALLET_ID = 'walletId',
   WALLET_IDS = 'walletIds',
   WALLET_IDS_AND_AMOUNTS = 'walletIdsAndAmounts',
+  PUZZLE_DECORATOR = 'puzzleDecorator',
 }
 
 export default WalletConnectCommandParamName;
