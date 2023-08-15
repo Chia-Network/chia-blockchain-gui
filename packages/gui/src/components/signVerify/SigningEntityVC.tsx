4328243d777fb6f021c7ebba2b2c82ec939cb017
Jeff Cruikshank <jeff@chia.net>
2023-08-15 09:46:54 -0700

    Demo support for adding proofs to VC signature message

--- packages/gui/src/components/signVerify/SigningEntityVC.tsx
+++ packages/gui/src/components/signVerify/SigningEntityVC.tsx
@@ -1,6 +1,7 @@
-import { CopyToClipboard, Flex, TextField } from '@chia-network/core';
-import { Trans } from '@lingui/macro';
-import { InputAdornment } from '@mui/material';
+import { useLocalStorage } from '@chia-network/api-react';
+import { CopyToClipboard, DropdownActions, Flex, MenuItem, TextField } from '@chia-network/core';
+import { Trans, t } from '@lingui/macro';
+import { Checkbox, InputAdornment, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
@@ -15,6 +16,7 @@ export type SigningEntityVCProps = {
 export default function SigningEntityVC(props: SigningEntityVCProps) {
   const { entityName, entityValueName } = props;
   const { getValues, setValue } = useFormContext();
+  const [proofs] = useLocalStorage<string[]>('proofs', []);
   const [vcLauncherId, setVCLauncherId] = useState<string | undefined>(getValues(entityValueName));
 
   function handleVCSelected(newVCLauncherId?: string) {
@@ -38,37 +40,67 @@ export default function SigningEntityVC(props: SigningEntityVCProps) {
   }, [entityName, entityValueName, setValue, getValues]);
 
   return (
-    <Flex flexDirection="column" gap={1}>
-      <VCDropdown
-        vcLauncherId={vcLauncherId}
-        onChange={handleVCSelected}
-        defaultTitle={<Trans>Select a Credential</Trans>}
-        variant="outlined"
-        color="primary"
-        // disabled={isLoading}
-        fullWidth
-      />
-      <TextField
-        label={<Trans>Credential Id</Trans>}
-        variant="filled"
-        name={entityValueName}
-        inputProps={{
-          'data-testid': 'signing-entity-vc-launcher-id',
-          readOnly: true,
-        }}
-        disabled
-        InputLabelProps={{
-          shrink: !!vcLauncherId,
-        }}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <CopyToClipboard value={vcLauncherId ?? ''} data-testid="signing-entity-vc-launcher-id-copy" />
-            </InputAdornment>
-          ),
-        }}
-        fullWidth
-      />
+    <Flex flexDirection="column" gap={2}>
+      <Flex flexDirection="column" gap={1}>
+        <VCDropdown
+          vcLauncherId={vcLauncherId}
+          onChange={handleVCSelected}
+          defaultTitle={<Trans>Select a Credential</Trans>}
+          variant="outlined"
+          color="primary"
+          // disabled={isLoading}
+          fullWidth
+        />
+        <TextField
+          label={<Trans>Credential Id</Trans>}
+          variant="filled"
+          name={entityValueName}
+          inputProps={{
+            'data-testid': 'signing-entity-vc-launcher-id',
+            readOnly: true,
+          }}
+          disabled
+          InputLabelProps={{
+            shrink: !!vcLauncherId,
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <CopyToClipboard value={vcLauncherId ?? ''} data-testid="signing-entity-vc-launcher-id-copy" />
+              </InputAdornment>
+            ),
+          }}
+          fullWidth
+        />
+      </Flex>
+      <Flex flexDirection="row" alignItems="center" flexGrow={1} gap={1}>
+        <Typography color="textPrimary">
+          <Trans>Include Proofs:</Trans>
+        </Typography>
+        <Flex flexDirection="column" flexGrow={1} gap={1}>
+          <DropdownActions
+            onSelect={() => {}}
+            label={t`Select Proofs`}
+            variant="outlined"
+            color="secondary"
+            // size="large"
+            fullWidth
+          >
+            {(proofs ?? []).map((proof) => (
+              <MenuItem onClick={() => {}} value={proof} selected={false} title={proof}>
+                <Checkbox
+                  disableRipple
+                  disableFocusRipple
+                  disableTouchRipple
+                  onChange={() => {}}
+                  sx={{ '&:hover': { background: 'none' }, backgroundColor: 'none' }}
+                />
+                {proof}
+              </MenuItem>
+            ))}
+          </DropdownActions>
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
