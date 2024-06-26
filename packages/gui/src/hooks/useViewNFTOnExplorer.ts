fd3e0174873c9f5ed6546456ad4da776c8716b60
ClydeWallace22 <48610606+BrandtH22@users.noreply.github.com>
2024-06-26 14:18:54 -0600

    update spacescan nft testnet link (#2402)

--- packages/gui/src/hooks/useViewNFTOnExplorer.ts
+++ packages/gui/src/hooks/useViewNFTOnExplorer.ts
@@ -11,7 +11,7 @@ function getMintGardenURL(nft: NFTInfo, testnet: boolean) {
 }
 
 function getSpacescanURL(nft: NFTInfo, testnet: boolean) {
-  const url = `https://spacescan.io/${testnet ? 'txch10' : 'xch'}/nft/${nft.$nftId}`;
+  const url = `https://${testnet ? 'testnet.' : ''}spacescan.io/nft/${nft.$nftId}`;
   return url;
 }
 
