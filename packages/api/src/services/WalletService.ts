99327c9e78142c460182c66768c39a3c81e88b6f
Izumi Hoshino <admin@chiamine.jp>
2024-10-08 00:24:22 +0900

    Added `offer_only` option to `create_offer_for_ids`

--- packages/api/src/services/WalletService.ts
+++ packages/api/src/services/WalletService.ts
@@ -245,6 +245,7 @@ export default class Wallet extends Service {
     validateOnly?: boolean;
     disableJSONFormatting?: boolean;
     maxTime?: number;
+    offerOnly?: boolean;
   }) {
     const { disableJSONFormatting, driverDict, ...restArgs } = args;
     return this.command<{ offer: string; tradeRecord: TradeRecord }>(
