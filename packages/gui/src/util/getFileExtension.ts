169ad6e923140460e05eb23b678fb8cd9a6f8148
Zlatko <zfedor@gmail.com>
2025-01-27 11:32:17 +0100

    use more secure way to get file extension add tests for function

--- packages/gui/src/util/getFileExtension.ts
+++ packages/gui/src/util/getFileExtension.ts
@@ -1,12 +1,29 @@
-export default function getFileExtension(url?: string): string | undefined {
+export default function getFileExtension(url?: string, allowedExtensions?: string[]): string | undefined {
   if (!url) {
     return undefined;
   }
 
-  const extension = url.split('.').pop();
-  if (extension && extension.indexOf('/') === -1) {
-    return extension.toLowerCase();
-  }
+  try {
+    const urlObj = new URL(url);
+    const pathname = decodeURIComponent(urlObj.pathname);
 
-  return undefined;
+    const lastSegment = pathname.substring(pathname.lastIndexOf('/') + 1);
+    if (!lastSegment || lastSegment.startsWith('.')) {
+      return undefined;
+    }
+
+    const dotIndex = lastSegment.lastIndexOf('.');
+    if (dotIndex === -1 || dotIndex === lastSegment.length - 1) {
+      return undefined;
+    }
+    
+    const extension = lastSegment.substring(dotIndex + 1).toLowerCase();
+    if (allowedExtensions && !allowedExtensions.includes(extension)) {
+      return undefined;
+    }
+
+    return extension;
+  } catch (e) {
+    return undefined;
+  }
 }
