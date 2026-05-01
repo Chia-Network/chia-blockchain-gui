b3870f5d2a029ecc0a3d714d5cb9f3efc9f4f228
Zlatko <zlatko@chia.net>
2026-05-01 10:37:46 +0200

    add new permission api

--- packages/gui/src/electron/constants/API.ts
+++ packages/gui/src/electron/constants/API.ts
@@ -6,6 +6,7 @@ enum API {
   ADDRESS_BOOK = 'addressBookAPI',
   LINK = 'linkAPI',
   WEBSOCKET = 'webSocketAPI',
+  PERMISSIONS = 'permissionsAPI',
 }
 
 export default API;
---
+++ packages/gui/src/electron/constants/PermissionsAPI.ts
@@ -0,0 +1,9 @@
+enum PermissionsAPI {
+  PAIR_LIST = 'permissions:pair:list',
+  PAIR_REGISTER = 'permissions:pair:register',
+  PAIR_EDIT = 'permissions:pair:edit',
+  PAIR_REVOKE = 'permissions:pair:revoke',
+  CHECK = 'permissions:check',
+}
+
+export default PermissionsAPI;
