fdd853c06e0d56d1ec013bf36ce265244977285c
Zlatko <zlatko@chia.net>
2026-05-05 17:27:50 +0200

    add get_current_address into innocuous commands

--- packages/gui/src/electron/permissions/commandCapabilities.ts
+++ packages/gui/src/electron/permissions/commandCapabilities.ts
@@ -41,6 +41,7 @@ const BALANCE_COMMANDS = new Set([
 const INNOCUOUS_COMMANDS = new Set([
   // Wallet / account info reads
   'chia_wallet.get_wallets', // gaming
+  'chia_wallet.get_current_address', // gaming
   'chia_wallet.get_next_address', // gaming
   'chia_wallet.get_sync_status',
   // Coin & blockchain reads
