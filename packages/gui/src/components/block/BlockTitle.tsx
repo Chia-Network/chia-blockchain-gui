e04e29752ce13e827029514af7d61d4df4174c2b
ClydeWallace22 <48610606+BrandtH22@users.noreply.github.com>
2026-09-10 17:11:35 -0600

    Fix block viewer (#3092)
    * fix block viewer regression
    
    * fix block view navigation
    
    * add fallback for invalid urls

--- packages/gui/src/components/block/BlockTitle.tsx
+++ packages/gui/src/components/block/BlockTitle.tsx
@@ -18,7 +18,7 @@ export default function BlockTitle(props: Props) {
   const navigate = useNavigate();
 
   function handleGoBack() {
-    navigate('/dashboard');
+    navigate('/dashboard/fullnode');
   }
 
   return (
